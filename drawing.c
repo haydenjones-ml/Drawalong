04b4cce95e0c98281d421cb2847fffa01025b119
Hayden Jones <hwjones90713@gmail.com>
2026-01-02 19:42:29 -0800

    fixed comments

--- drawing.c
+++ drawing.c
@@ -6,7 +6,6 @@ const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
 int main(int argc, char* argv[]) {
-    // 1. Initialization
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         printf("SDL Init failed: %s\n", SDL_GetError());
         return -1;
@@ -22,24 +21,19 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // 2. The Main Loop Setup
     SDL_Event windowEvent;
-    bool is_running = true; // Flag to keep the loop going
+    bool is_running = true;
 
-    // 3. The "Game Loop"
     while (is_running) {
-        // Handle ALL pending events for this frame
         while (SDL_PollEvent(&windowEvent)) {
-            // Check for the close button (X)
             if (windowEvent.type == SDL_QUIT) {
                 is_running = false;
             }
         }
         
-        // (Rendering code will go here later)
+        // RENDERING CODE HERE 
     }
 
-    // 4. Cleanup
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
